f7600794693e1160f3efd830024ac7f60a5879b1
Mriduul7621 <mr.mriduul@gmail.com>
2026-09-08 05:01:56 +0600

    Change bcrypt import to bcryptjs

--- server/routes/production.routes.ts
+++ server/routes/production.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { getPool, isDatabaseConfigured } from '../database/connection';
 import { fallbackStore, createId } from '../fallbackStore';
